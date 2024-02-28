fff50bc5ebf4c78525a8e10ec9d6e6fb08e94c50
Jennifer <jenniferpeterson@pursuit.org>
2024-02-28 11:53:29 -0500

    created a models folder

---
+++ models/transaction.model.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    id: 1,
+    item_name: "Salary",
+    amount: 3000,
+    date: "2024-02-28",
+    from: "Employer",
+    category: "Income",
+  },
+  {
+    id: 2,
+    item_name: "Rent",
+    amount: -1200,
+    date: "2024-02-28",
+    from: "Landlord",
+    category: "Housing",
+  },
+  {
+    id: 3,
+    item_name: "Groceries",
+    amount: -200,
+    date: "2024-02-27",
+    from: "Grocery Store",
+    category: "Food",
+  },
+  {
+    id: 4,
+    item_name: "Savings",
+    amount: 500,
+    date: "2024-02-28",
+    from: "Bank",
+    category: "Savings",
+  },
+];
